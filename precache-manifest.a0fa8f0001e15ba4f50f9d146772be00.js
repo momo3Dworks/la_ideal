3bfd313ba9e4f964f5d640be9dee1383116a724e
momotaroking <119371383+momo3Dworks@users.noreply.github.com>
2024-04-22 16:06:24 -0300

    First Commit

---
+++ precache-manifest.a0fa8f0001e15ba4f50f9d146772be00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8ff1ed2af6e621a17e59180ed231ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d95b9753c11281c54c8",
+    "url": "/static/css/main.135fb986.chunk.css"
+  },
+  {
+    "revision": "9d63d2863d265046772f",
+    "url": "/static/js/2.221a553e.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "4d95b9753c11281c54c8",
+    "url": "/static/js/main.f1a3568f.chunk.js"
+  },
+  {
+    "revision": "3dfe5f0477b66a36e469",
+    "url": "/static/js/runtime~main.a1b9b0b5.js"
+  },
+  {
+    "revision": "4a5a1db588ae699d1559f88851697ecf",
+    "url": "/static/media/LA IDEALLOGO 1.4a5a1db5.png"
+  },
+  {
+    "revision": "ecd4cd177f799a43f01e7a186aa640ed",
+    "url": "/static/media/LA IDEALLOGO 2.ecd4cd17.png"
+  }
+]);
